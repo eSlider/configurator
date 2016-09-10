65a48d90ca96214edde3ceab58c3c39d1ddf0f57
Andriy Oblivantsev <eslider@gmail.com>
2016-09-10 12:57:59 +0200

    Add data item search filter

---
+++ Entity/DataItemSearchFilter.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Mapbender\ConfiguratorBundle\Entity;
+
+/**
+ * Class DataItemSearchFilter
+ *
+ * @package Mapbender\ConfiguratorBundle\Entity
+ * @author  Andriy Oblivantsev <eslider@gmail.com>
+ */
+class DataItemSearchFilter extends DataItem
+{
+    const FETCH_ONE_WITHOUT_CHILDREN  = 0;
+    const FETCH_ONE_AND_CHILDREN      = 1;
+    const FETCH_MANY_WITHOUT_CHILDREN = 2;
+    const FETCH_MANY_AND_CHILDREN     = 3;
+
+    /** @var array */
+    protected $fields;
+    protected $fetchMethod = self::FETCH_ONE_AND_CHILDREN;
+
+    /** @var int limit fetching results */
+    protected $fetchLimit;
+
+    /**
+     * @param mixed $fields
+     */
+    public function setFields($fields)
+    {
+        $this->fields = $fields;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFields()
+    {
+        return $this->fields;
+    }
+
+    /**
+     * @param int $fetchMethod
+     * @return DataItemSearchFilter
+     */
+    public function setFetchMethod($fetchMethod)
+    {
+        $this->fetchMethod = $fetchMethod;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getFetchMethod()
+    {
+        return $this->fetchMethod;
+    }
+
+    /**
+     * @return bool
+     */
+    public function shouldFetchChildren()
+    {
+        return $this->fetchMethod === static::FETCH_MANY_AND_CHILDREN
+        || $this->fetchMethod === static::FETCH_ONE_AND_CHILDREN;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getFetchLimit()
+    {
+        return $this->fetchLimit;
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasLimit()
+    {
+        return $this->fetchLimit !== null;
+    }
+
+    /**
+     * @param int|null $fetchLimit
+     */
+    public function setFetchLimit($fetchLimit)
+    {
+        $this->fetchLimit = $fetchLimit;
+    }
+}
